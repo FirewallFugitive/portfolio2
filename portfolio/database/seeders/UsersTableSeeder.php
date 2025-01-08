fdf9e744c510569d969c77f6998a244b567ed7d9
lm00ns <lucasmoons@hotmail.com>
2025-01-08 21:55:09 +0100

    seeder change

--- portfolio/database/seeders/UsersTableSeeder.php
+++ portfolio/database/seeders/UsersTableSeeder.php
@@ -12,6 +12,13 @@ class UsersTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('users')->insert([
+            'name' => 'test',
+            'email' => 'test@example.com',
+            'password' => Hash::make('testtest'),
+            'is_admin' => false,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 }
